8cd2ab53bf6b81ff385e8a405b3ef14b4ec805e6
ebertrand <ebertrand@gaming.tech>
2022-11-09 14:44:55 +0100

    working on TTF

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL_ttf.h>
 
 #undef main
+
+#include "click.h"
 
 #define WIDTHGAME 540
 #define HEIGHTWINDOW 630
@@ -12,9 +15,29 @@
 using namespace std;
 
 int main(void) {
+	SDL_Event event;
+	bool quit = false;
+	Click clik;
 	MainSDLWindow* sdlwin = new MainSDLWindow;
 	if (sdlwin->Init("BabyMiam", WIDTHGAME, HEIGHTWINDOW) == EXIT_FAILURE)
 	{
 		exit(EXIT_FAILURE);
+	}
+
+	TTF_Init();
+
+	int exit = 0;
+
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+		clik.EventManager();
 	}
 }
